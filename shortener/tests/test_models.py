630f8873330b52c658a697f42087bdc65ba31e49
Krzysztof Adamski <krzysztof.adamski@britenet.com.pl>
2021-03-05 04:28:24 +0100

    dodane testy modelu .. kilka

---
+++ shortener/tests/test_models.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from shortener.models import Shortcut
+from django.conf import settings
+
+
+class ShortcutModelTests(TestCase):
+    def setUp(self):
+        self.url = "http://wp.pl"
+        self.link = "wppl"
+        Shortcut.objects.create(url=self.url, link=self.link)
+
+    def test_should_create_shortcut(self):
+        """Shortcut should be created."""
+        shortcut = Shortcut.objects.get(url=self.url)
+        self.assertEqual(shortcut.url, self.url)
+
+    def test_shortcut_str(self):
+        """Shortcut object str."""
+        shortcut = Shortcut.objects.get(url=self.url)
+        self.assertEqual(shortcut.__str__(), self.url)
+
+    def test_shortcut_absolute_url(self):
+        """Shortcut object absolute url."""
+        shortcut = Shortcut.objects.get(url=self.url)
+        self.assertEqual(shortcut.get_absolute_url(), f"/{self.link}/")
+
+    def test_shortcut_link_url(self):
+        """Shortcut object link url."""
+        shortcut = Shortcut.objects.get(url=self.url)
+        expected_url = f"{settings.BASE_URL}/{self.link}"
+        self.assertEqual(shortcut.get_link_url, expected_url)
