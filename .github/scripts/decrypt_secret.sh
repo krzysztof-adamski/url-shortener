ba0d4de08225ce858f76ab39c27278a55f8d9d3c
Krzysztof Adamski <krzysztof.adamski@britenet.com.pl>
2021-03-29 17:18:22 +0200

    decoder

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,7 +4,11 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$DIGITALOCEAN_HOST" --output $HOME/secrets/dghost.json dghost.json.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$DIGITALOCEAN_KEY" --output $HOME/secrets/dgkey.json dgkey.json.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$DIGITALOCEAN_USER" --output $HOME/secrets/dguser.json dguser.json.gpg
+#gpg --quiet --batch --yes --decrypt --passphrase="$DIGITALOCEAN_HOST" --output $HOME/secrets/dghost.json dghost.json.gpg
+#gpg --quiet --batch --yes --decrypt --passphrase="$DIGITALOCEAN_KEY" --output $HOME/secrets/dgkey.json dgkey.json.gpg
+#gpg --quiet --batch --yes --decrypt --passphrase="$DIGITALOCEAN_USER" --output $HOME/secrets/dguser.json dguser.json.gpg
+
+echo $DIGITALOCEAN_HOST > $HOME/secrets/dghost.json
+echo $DIGITALOCEAN_KEY > $HOME/secrets/dgkey.json
+echo $DIGITALOCEAN_USER > $HOME/secrets/dguser.json
 
